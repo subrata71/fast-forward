5bf2d9caec6e1abf9b755206121ec9afdc41d243
Subrata Pappu <subrata@appsmith.com>
2023-11-09 05:20:57 +0000

    a

---
+++ pages/Page1/jsobjects/JSObjectOne/JSObjectOne.js
@@ -0,0 +1,12 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFunTwoT () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
